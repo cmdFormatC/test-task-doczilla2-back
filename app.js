5b08e825772e10936693806fb0fede03561f0e12
cmdFormatC <117574451+cmdFormatC@users.noreply.github.com>
2024-02-11 20:57:16 +0300

    [~] add cors permissions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { handleErrorConstructor } = require('./utils/handleErrorTools');
 const { handleError } = require('./middlewares/handleError');
@@ -8,6 +9,7 @@ const { handleError } = require('./middlewares/handleError');
 const { PORT = 3030, BASE_PATH } = process.env;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// const { celebrate, Joi, Segments } = require('celebrate');
 
 const {
   getStudents,
